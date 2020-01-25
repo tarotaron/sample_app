268920649b9a25e6960745511e2e342b6ca368de
tarotaron <suzu9095@gmail.com>
2020-01-25 20:17:56 +0900

    finish

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+Refile.secret_key = 'e6a49c940ae0b1002082cd85190a364bff937fc29004f4cecdab546ad4fc4ccece8161fe6a335ed050a7aa9af001af110baf2b485afb2f747e1aa00093b99595'
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
